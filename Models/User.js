dbc7cb15d683ff17ce6903044537f970e93b50aa
GustavSoderberg <gustav.soderberg@outlook.com>
2022-10-16 10:35:34 +0200

    MapManager is now a class + cleaned up some code

--- Models/User.js
+++ Models/User.js
@@ -1,24 +1,3 @@
-// import { BackHandler } from "react-native"
-
-// class User {
-
-//     name = String()
-//     lat = String()
-//     long =  String()
-
-//     constructor(name, lat, long) {
-//         this.name = name
-//         this.lat = lat
-//         this.long = long
-//     }
-
-// }
-
-// const user = new User("Jonas", 59.360931, 17.959703)
-
-// export default user
-
-
 export const User = (name, lat, long) => { 
     return { 
         name: name, 
